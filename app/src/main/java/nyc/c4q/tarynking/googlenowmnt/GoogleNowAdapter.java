51b3883d6422571ef49d90bed5a7f11cb3070f57
tking2005 <tking2005@gmail.com>
2016-10-30 16:53:10 -0400

    first commit

---
+++ app/src/main/java/nyc/c4q/tarynking/googlenowmnt/GoogleNowAdapter.java
@@ -0,0 +1,25 @@
+package nyc.c4q.tarynking.googlenowmnt;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+/**
+ * Created by tarynking on 10/30/16.
+ */
+
+public class GoogleNowAdapter extends RecyclerView.Adapter {
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
