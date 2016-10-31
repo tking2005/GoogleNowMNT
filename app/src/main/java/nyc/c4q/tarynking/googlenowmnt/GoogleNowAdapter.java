62d3e3a9c9e135d8be9a2930cc8881d192307f27
Nesada <knesada@yahoo.com>
2016-10-31 18:55:38 -0400

    Updated with CardView

--- app/src/main/java/nyc/c4q/tarynking/googlenowmnt/GoogleNowAdapter.java
+++ app/src/main/java/nyc/c4q/tarynking/googlenowmnt/GoogleNowAdapter.java
@@ -1,25 +1,55 @@
 package nyc.c4q.tarynking.googlenowmnt;
 
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 /**
  * Created by tarynking on 10/30/16.
  */
 
-public class GoogleNowAdapter extends RecyclerView.Adapter {
-    @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+public class GoogleNowAdapter extends RecyclerView.Adapter<GoogleNowAdapter.CardHolder> {
+    private LayoutInflater mInflater;
+
+
+    public GoogleNowAdapter(Context context) {
+        mInflater = LayoutInflater.from(context);
     }
 
     @Override
-    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+    public CardHolder onCreateViewHolder(ViewGroup parent, int viewType) {
 
+        View view = mInflater.inflate(R.layout.test_card_view2,parent,false);
+        CardHolder holder = new CardHolder(view);
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(CardHolder holder, int position) {
+
+        Log.d("Yes", ""+position);
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return 3;
     }
+
+    //this class is going to keep one drop in a current moment
+    public static class CardHolder extends RecyclerView.ViewHolder {
+        TextView mWhat;
+
+        //class constractor
+        public CardHolder(View itemView) {
+            super(itemView);
+
+        }
+
+
+    }
+
 }
--- app/src/main/java/nyc/c4q/tarynking/googlenowmnt/MainActivity.java
+++ app/src/main/java/nyc/c4q/tarynking/googlenowmnt/MainActivity.java
@@ -2,12 +2,27 @@ package nyc.c4q.tarynking.googlenowmnt;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
+    RecyclerView mRecyclerView;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mRecyclerView = (RecyclerView)findViewById(R.id.mRecyclerView);
+
+        LinearLayoutManager manager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(manager);
+        mRecyclerView.setAdapter(new GoogleNowAdapter(this));
+
+
+
     }
 }
